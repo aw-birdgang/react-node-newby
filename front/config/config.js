4030995d202c7645a1323bb3139a096b266e1e82
seonggyun <birdgang82@gmail.com>
2022-03-20 18:03:40 +0900

    add config dir

---
+++ front/config/config.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
+// export const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.birdgang.com' : 'http://54.95.151.72/';
+// eslint-disable-next-line import/prefer-default-export
+export const backUrl = 'http://54.95.151.72';
