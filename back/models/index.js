74819e07b29227b5ec6e87a9119ba38978f894b1
seonggyun <birdgang82@gmail.com>
2022-03-20 15:11:37 +0900

    first commit

---
+++ back/models/index.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const Comment = require('./comment');
+const Hashtag = require('./hashtag');
+const Image = require('./image');
+const Post = require('./post');
+const User = require('./user');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+);
+
+db.Comment = Comment;
+db.Hashtag = Hashtag;
+db.Image = Image;
+db.Post = Post;
+db.User = User;
+
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+});
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
